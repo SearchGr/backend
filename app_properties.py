2f0774ff5335ac65eb38a6ee42aa7addb3c9a532
Ilinca Asandului <antonia.ilinca7@yahoo.com>
2021-02-02 22:57:28 +0200

    Refactoring

--- app_properties.py
+++ app_properties.py
@@ -4,6 +4,6 @@ encryption_key = b'4n4aP12ECqHN-ipG70WTjwY3Q4ETXs42FJpkfOugCwk='
 flask_app_secret = "super secret key"
 redirect_url = "http://localhost:4200/search"
 redirect_home = "http://localhost:4200/home"
-callback_url = "https://b38332cbac73.ngrok.io/callback"
+callback_url = "https://a83ef8c7e2ec.ngrok.io/callback"
 classification_threshold = 0.6
 detection_threshold = 0.6
